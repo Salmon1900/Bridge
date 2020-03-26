958da9c609973a95546f2d06d20417ffa3c1e5e2
Idan <idansalmon@gmail.com>
2020-03-26 17:00:11 +0200

    Added git ignore

---
+++ Server/Bridgette/src/main/java/birdgette/Controlles/ExampleController.java
@@ -0,0 +1,15 @@
+package birdgette.Controlles;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ExampleController {
+
+    @GetMapping("/test")
+    public String testReq(){
+        return "Hello World";
+    }
+}
